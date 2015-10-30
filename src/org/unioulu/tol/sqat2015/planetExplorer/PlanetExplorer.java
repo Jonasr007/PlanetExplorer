497dead5891187188d3cd07131644c1ad1405ae7
Simon <sermler@mail.uni-mannheim.de>
2015-10-30 17:22:05 +0200

    Project finished

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -1,19 +1,155 @@
 package org.unioulu.tol.sqat2015.planetExplorer;
 
+import java.util.ArrayList;
+
 // Before submitting write your ID and finish time here. Your ID is written on project description sheets.
-// ID:
-// Finish time:
+// ID: 155
+// Finish time: 17:19
+
 public class PlanetExplorer {
+	// size of the field
+	private int x;
+	private int y;
+	
+	public int getXp() {
+		return xp;
+	}
+
+	public int getYp() {
+		return yp;
+	}
+
+	public int getD() {
+		return d;
+	}
+
+	// List of obstacles 
+	ArrayList<Integer> obstaclesList; 
+	
+	// currentPosition of explorer
+	int xp;
+	int yp;
+	
+	//oldPosition of explorer
+	int oldXp;
+	int oldYp;
+	
+	// direction of the explorer (1=east, 2=south, 3=west, 4=north)
+	int d;
+	
 	public PlanetExplorer(int x, int y, String obstacles){
+		this.x=x;
+		this.y=y;
+		
+		// starts from (0,0) and fase to north
+		xp=0;
+		yp=0;
+		oldXp=0;
+		oldYp=0;
+		d=4;
+		
+		obstaclesList = new ArrayList<Integer>();
+		
+		// remove first and last letter
+		obstacles=obstacles.substring(1, obstacles.length()-1);
+		
+		String[] pairs = obstacles.split("\\)\\(");
+		
+		for (int i=0; i<pairs.length; i++) {
+			String[] pair = pairs[i].split(",");
+			obstaclesList.add(Integer.parseInt(pair[0]));			
+			obstaclesList.add(Integer.parseInt(pair[1]));
+		}				
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
 	 *  
-		Example use:
+		Example use:explorer.getX()
 		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")  //A 100x100 grid with two obstacles at coordinates (5,5) and (7,8) 
 	 */
 	}
 	
+	public void exexuteOneCommand(char c) {
+		// direction of the explorer (1=east, 2=south, 3=west, 4=north)
+		this.oldXp=xp;
+		this.oldYp=yp;
+		switch(d) {
+		case 1:
+			if(c=='r')
+				d=2;
+			if(c=='l')
+				d=4;
+			if(c=='f'&&!NoBoarder())
+				xp+=1;
+			if(c=='b'&&!NoBoarder())
+				xp-=1;			
+		break;
+		case 2:
+			if(c=='r')
+				d=3;
+			if(c=='l')
+				d=1;
+			if(c=='f'&&!NoBoarder())
+				yp-=1;
+			if(c=='b'&&!NoBoarder())
+				yp+=1;			
+		break;
+		case 3:
+			if(c=='r')
+				d=4;
+			if(c=='l')
+				d=2;
+			if(c=='f'&&!NoBoarder())
+				xp-=1;
+			if(c=='b'&&!NoBoarder())
+				xp+=1;
+		break;
+		case 4:
+			if(c=='r')
+				d=1;
+			if(c=='l')
+				d=3;
+			if(c=='f'&&!NoBoarder())
+				yp+=1;
+			if(c=='b'&&!NoBoarder())
+				yp-=1;			
+		break;
+		default:
+			System.out.println("Error Invalid direction");
+		}
+		NoObstacle();
+	}
+	
+	public boolean NoBoarder() {
+		if(xp==x||yp==y) {
+			System.out.println("Invalid move, Explorer cross the border");
+			xp=oldXp;
+			yp=oldYp;
+		
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean NoObstacle() {
+		for(int i=0; i<obstaclesList.size(); i+=2) {
+			if(xp==obstaclesList.get(i)&&yp==obstaclesList.get(i+1)) {
+				System.out.println("Invalid move, Explorer cross tNoObstacle()NoObstacle()he obstacle");
+				xp=oldXp;
+				yp=oldYp;
+			}
+			return false;
+		}
+		return false;
+	}
+	
 	public String executeCommand(String command){
+		char [] commands = command.toCharArray();
+		
+		for(int i=0; i<commands.length; i++) {
+			exexuteOneCommand(commands[i]);
+			System.out.println("(" + xp + "," + + yp + ")" + " d= " +d);
+		}
+		
 		
 		/* The command string is composed of "f" (forward), "b" (backward), "l" (left) and "r" (right)
 		 * Example: 
@@ -24,7 +160,18 @@ public class PlanetExplorer {
 		 * Where pos_x and pos_y are the final coordinates, facing is the current direction the explorer is pointing to (N,S,W,E).
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
-		
-		return null;
+		return "(" + xp +"," + yp + ")" + "Richtung: " + d + "valid Move";
+	}
+
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public ArrayList getObstaclesList() {
+		return obstaclesList;
 	}
 }
--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -3,11 +3,55 @@ package org.unioulu.tol.sqat2015.planetExplorer.tests;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.unioulu.tol.sqat2015.planetExplorer.PlanetExplorer;
 
 public class TestPlanetExplorer {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		testInit();
+		testWalkingWithoutObstacles();
+//		testWalkingOnABorder();	
 	}
+	
+	@Test
+	public void testInit() {
+		PlanetExplorer explorer = new PlanetExplorer(10,10,"(5,5)(4,3)(7,8)");  //A 100x100 grid with two obstacles at coordinates (5,5) and (7,8) 
+		assertEquals(explorer.getX(), 10);
+		assertEquals(explorer.getY(), 10);
+		assertEquals(explorer.getXp(), 0);
+		assertEquals(explorer.getYp(), 0);
+		assertEquals(explorer.getD(), 4);		
+		assertEquals(explorer.getObstaclesList().get(0), 5);
+		assertEquals(explorer.getObstaclesList().get(1), 5);
+		assertEquals(explorer.getObstaclesList().get(2), 4);		
+		assertEquals(explorer.getObstaclesList().get(3), 3);
+		assertEquals(explorer.getObstaclesList().get(4), 7);
+		assertEquals(explorer.getObstaclesList().get(5), 8);
+	}
+
+	@Test
+	public void testWalkingWithoutObstacles() {
+		PlanetExplorer explorer = new PlanetExplorer(10,10,"(5,5)(4,3)(7,8)");  //A 100x100 grid with two obstacles at coordinates (5,5) and (7,8) 
+		explorer.executeCommand("ffrf");
+		assertEquals(explorer.getXp(), 1);
+		assertEquals(explorer.getYp(), 2);		
+	}
+
+	@Test
+	public void testWalkingOnABorder() {
+		PlanetExplorer explorer = new PlanetExplorer(5,5,"(4,4)(4,3)");  //A 100x100 grid with two obstacles at coordinates (5,5) and (7,8) 
+		explorer.executeCommand("ffff");
+		assertEquals(explorer.getXp(), 0);
+		assertEquals(explorer.getYp(), 4);		
+	}
+	
+	@Test
+	public void testWalkingOnAOstacle() {
+		PlanetExplorer explorer = new PlanetExplorer(5,5,"(0,1)(4,3)");  
+		assertEquals(explorer.getXp(), 0);
+		assertEquals(explorer.getYp(), 0);
+	}
+	
+	
 }
